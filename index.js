eb964af116d54d73c3cd2fb72f5a4071ba114577
PedroHenriqueMelo1 <you@example.comew>
2024-07-07 20:50:25 -0300

    teste v2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Telegraf} = require('telegraf');
- const bot = new Telegraf('ENV_BOT');
+ const bot = new Telegraf('7330295245:AAGxQq2ZhEHgnMJcOmvbaSFL6lL7jtTntF4');
 const { Start } = require("./bot/handlers/Start")
 const { RunDb } = require("./bot/db");
  
