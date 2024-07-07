492db355991078ca5cb1449ea16c506023b8dfa1
Pedro Henrique Almeida <151796879+PedroHenriqueMelo1@users.noreply.github.com>
2024-07-07 14:38:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Telegraf} = require('telegraf');
- const bot = new Telegraf('7330295245:AAGxQq2ZhEHgnMJcOmvbaSFL6lL7jtTntF4');
+ const bot = new Telegraf('ENV_BOT');
 const { Start } = require("./bot/handlers/Start")
 const { RunDb } = require("./bot/db");
  
